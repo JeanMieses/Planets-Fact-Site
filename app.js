e0f8769d1646b15ce31394ca8f8c46f0f1f225c5
JeanMieses <araujo0275@gmail.com>
2021-07-17 14:16:32 -0400

    Adding layouts for different screen size

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const router = require('./routers/router');
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use(express.static('assets'));  
 // ...
 app.use('', router);
 
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,1 +1,27 @@
+const overviewBtn = document.querySelectorAll('.overview-btn');
+const structureBtn = document.querySelectorAll('.structure-btn');
+const surfaceBtn = document.querySelectorAll('.surface-btn');
 
+const overview = document.querySelector('#overview');
+const structure = document.querySelector('#structure');
+const surface = document.querySelector('#surface');
+
+for(let i = 0; i < 2; i++) {
+  overviewBtn[i].addEventListener('click', () => {
+    currentFact(overview, structure, surface);
+  })
+
+  structureBtn[i].addEventListener('click', () => {
+    currentFact(structure, overview, surface);
+  })
+
+  surfaceBtn[i].addEventListener('click', () => {
+    currentFact(surface, structure, overview);
+  })
+}
+
+function currentFact(element1, element2, element3) {
+  element1.classList.remove('hide');
+  element2.classList.add('hide');
+  element3.classList.add('hide');
+}
