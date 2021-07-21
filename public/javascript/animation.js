e9a326ba7e1b4f6179b0584c4d80da6ff56cef81
JeanMieses <araujo0275@gmail.com>
2021-07-21 12:52:37 -0400

    Adding animation to the background

---
+++ public/javascript/animation.js
@@ -0,0 +1,38 @@
+const h1_ = document.querySelector('h1');
+const spans = document.querySelectorAll('span');
+const planet = document.querySelector('#planet');
+
+//This will add an animation to the numbers on the bottom page
+for (let i = 1; i < spans.length; i++) {
+  anime({
+    targets: spans[i],
+    innerHTML: [0, spans[i].innerText],
+    easing: 'linear',
+    duration: 1600,
+    round: 10 // Will round the animated value to 1 decimal
+  });
+}
+
+anime({
+  targets: planet,
+  translateX: [-580, 0], // from 100 to 250
+  translateY: [1250, 0], // from 100 to 250
+  duration: 1230,
+  delay: 500,
+  scale: [{
+      value: 2,
+      easing: 'easeOutSine',
+      duration: 180
+    },
+    {
+      value: 0.8,
+      easing: 'easeOutSine',
+      duration: 180
+    },
+    {
+      value: 1,
+      easing: 'easeInOutQuad',
+      duration: 180
+    }
+  ]
+});
